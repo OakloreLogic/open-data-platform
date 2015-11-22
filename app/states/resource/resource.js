06dd23fa1af153ebe6f330de08cdd238e2d8169f
Karlo Colegio <kdqcolegio@gmail.com>
2015-11-22 10:58:24 +0800

    major update

--- app/states/resource/resource.js
+++ app/states/resource/resource.js
@@ -17,18 +17,51 @@
 	}).controller("ResourceController", function($scope, $stateParams, ResourceService) {
 		var resource = ResourceService.getResource($stateParams.id);
 		resource.then(function(result) {
-			$scope.resource = result;	
+			$scope.resource = result;
 			var jsonPath = "data/" + $scope.resource.resource_category + "/" + $scope.resource.resource_dataset + "/" +  $scope.resource.resource_name + ".json";
+			$scope.jsonPath = jsonPath;
 			$.getJSON(jsonPath, function(data) {
 				$.each(data, function(key, val) {
 					var tmpChartLabels = [];
 					var tmpChartSeries = [];
 					var chartLabels = [];
 					var chartSeries = [];
+					var options = {
+						axisY: {
+							offset: 80,
+							labelInterpolationFnc: function(value) {
+								return value + ' php';
+							}
+						},
+						series: {
+							'data': {lineSmooth: Chartist.Interpolation.none()},
+							'prediction': {lineSmooth: Chartist.Interpolation.simple()}
+						},
+						lineSmooth: Chartist.Interpolation.cardinal({
+						    fillHoles: true,
+						}),
+					};
+
+
+					var mediaQueries = [
+						["screen and (max-width:750px)", {
+							axisY: {
+								showLine: false,
+								showLabel: false,
+								offset: 0
+							},
+							axisX: {
+								showLine: false,
+								showLabel: false,
+							},
+							showPoint: false
+						}]
+					];
+
 					$(".chart-area").append("<div class=\"ct-chart ct-perfect-fourth ct_" + key + "\"></div>");
 					$.each(val, function(key2, val2) {
 						if(key2 == "title") {
-							$(".ct-chart.ct_"+key).append("<div>" + val2 + "</div>");
+							$(".ct-chart.ct_"+key).append("<div class='chart title'>" + val2 + "</div>");
 						} else {
 							$.each(val2, function(key3, val3) {
 								var i = 0;
@@ -43,6 +76,26 @@
 							});
 						}
 					});
+					var a, diff, tmp = 0, trendSkip = Math.round(tmpChartLabels.length/6);
+					var predictions = [];
+					var trend = [];
+					for(a = 0; a < tmpChartLabels.length; a++) {
+						predictions[a] = null;
+						trend[a] = null;
+					}
+
+					for(a = 0; a < tmpChartLabels.length; a++) {
+						if((a % trendSkip) == 0) {
+							trend[a] = tmp / (trendSkip - 1);
+							tmp = 0;
+						}
+						tmp += tmpChartSeries[a];
+					}
+					trend[0] = tmpChartSeries[0];
+					trend[tmpChartLabels.length-1] = tmpChartSeries[tmpChartLabels.length-1];
+					predictions[tmpChartLabels.length-1] = tmpChartSeries[tmpChartLabels.length-1];
+					diff = tmpChartSeries[tmpChartLabels.length-1] - tmpChartSeries[tmpChartLabels.length-2];
+					predictions[tmpChartLabels.length] = ((predictions[tmpChartLabels.length-1] + diff) + trend[tmpChartLabels.length-1])/2;
 					if($scope.resource.resource_format == "Line Graph") {
 						var j;
 						for(j = 0; j < tmpChartLabels.length; j++) {
@@ -50,14 +103,18 @@
 							chartSeries[j] = tmpChartSeries[j];
 						}
 						chartLabels[0] = tmpChartLabels[0];
+						chartLabels[tmpChartLabels.length] = "prediction";
 						chartLabels[tmpChartLabels.length-1] = tmpChartLabels[tmpChartLabels.length-1];
+						chartSeries[tmpChartLabels.length] = null;
 
 						new Chartist.Line(".ct-chart.ct_"+key, {
 						  	labels: chartLabels,
 						  	series: [
-						    	chartSeries
+						  		chartSeries,
+						  		trend,
+						    	predictions
 							]
-						});
+						}, options, mediaQueries);
 					} else {
 						var j;
 						for(j = 0; j < tmpChartLabels.length; j++) {
@@ -70,7 +127,7 @@
 						  	series: [
 						    	chartSeries
 							]
-						});
+						}, options, mediaQueries);
 					}
 				});
 			});
